837ec64a098bb9d90610aaaa79c4804529eb3740
michelleob <michelleobryan123@gmail.com>
2016-11-10 12:40:14 -0500

    Update Rear_Motor.ino

--- Rear_Motor_Team/Rear_Motor.ino
+++ Rear_Motor_Team/Rear_Motor.ino
@@ -1,14 +1,14 @@
 #define in_pin 11 //hall sensor pulse 
 #define pwm_rear 8 //rear motor PWM pin
 #define v_pin 63
-float pwm = 165;
+float pwm = 130;
 float tOld= 0;
 float tNew= 0; 
 float T=0;
 float w= 0; 
 int sampleLength = 100;
 int count = 0; 
-
+int minT = 11000;
 void setup() {
   Serial.begin(9600);
   pinMode(in_pin, INPUT);
@@ -18,20 +18,30 @@ void setup() {
 }
 
 void loop() {
-  T= tNew - tOld;
-  w= 1/(28*T)*1E6;
-  Serial.println(w);
   }
 
 
 void getT() {
-  if (count != sampleLength){
+    int tOldtemp= 0;
+    int tNewtemp= 0;
+    int Ttemp = 0;
+   tOldtemp = tNew;
+   tNewtemp = micros();
+   Ttemp= (tNewtemp - tOldtemp);
+   if(Ttemp<minT){
+    
+   }
+  else if (count != sampleLength){
     count++;
-  }
+    }
   else{
    count = 0; 
-    tOld = tNew;
-    tNew = micros();
+   tOld = tNew;
+   tNew = micros();
+   T= (tNew - tOld);
+   w= 1E6/(28*T);
+   Serial.print(w);
+   Serial.print('\n');
   }
  
-}
+}[
