32fb8fc72bb159d080a279ce816853db5caff235
Kenneth fang <Kenneth fang>
2016-11-20 13:45:29 -0500

    I added extra testing features: should automate testing for pwms ranged 95-180

--- Rear_Motor_Team/Rear_Motor/Rear_Motor.ino
+++ Rear_Motor_Team/Rear_Motor/Rear_Motor.ino
@@ -6,15 +6,20 @@ float tOld= 0;
 float tNew= 0; 
 float T=0;
 float w= 0; 
-int sampleLength = 10;
+int sampleLength = 25;
 int count = 0; 
 int x=30;
 int minT = 0;
+
+int tc = 0; //Counts number of data points collected on this PWM
+int ds = 100; //Number of data points to collect for each PWM
+
 void setup() {
   
   Serial.begin(9600);
   pinMode(in_pin, INPUT);
   pinMode(pwm_rear, OUTPUT);
+  
   while (x <pwm){ //Ramps up speed- Workaround for rear motor safety features
     analogWrite(pwm_rear, x);  
     delay(100);
@@ -24,11 +29,25 @@ void setup() {
   Serial.print("Effective Voltage: ");
   Serial.print(voltage);
   Serial.print("\n");
-  //analogWrite(pwm_rear, pwm);
+  analogWrite(pwm_rear, pwm);
   attachInterrupt(digitalPinToInterrupt(in_pin), getT, RISING); //Interrupt
 }
 
 void loop() {
+  if(tc == ds && pwm<180){
+    tc = 0;
+    
+    float voltage = analogRead(63)/14.2*pwm/180;
+    Serial.print("Effective Voltage: ");
+    Serial.print(voltage);
+    Serial.print("\n");
+    
+    pwm++;
+    analogWrite(pwm_rear, pwm);
+    Serial.print("Current PWM: ");
+    Serial.print(pwm);
+    Serial.print("\n");
+  }
   }
 
 
