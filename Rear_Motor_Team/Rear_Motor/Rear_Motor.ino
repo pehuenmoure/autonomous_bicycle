a33fcf05088a014d52697c93b3fbfe7293ba38ba
heyheyhay <hjs225@cornell.edu>
2016-11-20 12:56:18 -0500

    Update Rear_Motor.ino

--- Rear_Motor_Team/Rear_Motor/Rear_Motor.ino
+++ Rear_Motor_Team/Rear_Motor/Rear_Motor.ino
@@ -1,54 +1,50 @@
 #define in_pin 11 //hall sensor pulse 
 #define pwm_rear 8 //rear motor PWM pin
 #define v_pin 63
-float pwm = 60;
+float pwm = 95;
 float tOld= 0;
 float tNew= 0; 
 float T=0;
 float w= 0; 
-int sampleLength = 100;
+int sampleLength = 10;
 int count = 0; 
-int minT = 11000;
-
-//test count variable- Kenneth additions
-int tc = 0;
-int datasize = 300;
-
-
+int x=30;
+int minT = 0;
 void setup() {
+  
   Serial.begin(9600);
   pinMode(in_pin, INPUT);
-  pinMode(63,INPUT);
   pinMode(pwm_rear, OUTPUT);
-  analogWrite(pwm_rear, pwm);
-  Serial.print(
+  while (x <pwm){ //Ramps up speed- Workaround for rear motor safety features
+    analogWrite(pwm_rear, x);  
+    delay(100);
+    x=x+1;
+  }
+  float voltage = analogRead(63)/14.2*pwm/180;
+  Serial.print("Effective Voltage: ");
+  Serial.print(voltage);
+  Serial.print("\n");
+  //analogWrite(pwm_rear, pwm);
   attachInterrupt(digitalPinToInterrupt(in_pin), getT, RISING); //Interrupt
 }
 
 void loop() {
-  if(tc == datasize){
-    tc = 0;
-    pwm++;
-    analogWrite(pwm_rear,pwm);
-    Serial.print("Switching to pwm ");
-    Serial.print(pwm);
-    Serial.print('\n');
-  }
   }
 
 
 void getT() {
-  tc++;
-   if (count != sampleLength){
+   //Getting Average
+  if (count != sampleLength){
     count++;
     }
-   else{
+  else{ // Count == sampleLength
    count = 0; 
    tOld = tNew;
    tNew = micros();
    T= (tNew - tOld);
-   w= 1E6/(28*T);
+   w= (1E6)*sampleLength/(28*T);
    Serial.print(w);
    Serial.print('\n');
   }
+ 
 }
