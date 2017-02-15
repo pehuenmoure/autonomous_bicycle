38d1b02a99177ba0a424ab9106e102369d663f86
Kenneth fang <kwf37@cornell.edu>
2017-02-15 18:23:36 -0500

    Redid file writing and went back to printing strings instead of writing because I could not understand how to interpret bytes. I also readded the prompt so that one can type 'save' and it will save the file

--- web/server/app.js
+++ web/server/app.js
@@ -4,118 +4,42 @@ var http = require('http');
 var server = http.Server(app);
 var io = require('socket.io')(server);
 var SerialPort = require('serialport');
-
-//For Writing Data
-var fileWriter = require('msexcel-builder');
-var testNo = 0;
-var name = new String("data");
-var fileName = name.concat(String(testNo)).concat('.csv');
-var row = 1;
-var col = 1;
-console.log(fileName);
-
+var fs = require('fs');
 //For getting input- Will be edited out once site is nicer
 var prompt = require('prompt');
 prompt.start();
-// setPrompt();
 
-//======================rest of code=======================
 app.set('view engine', 'ejs');
+
+setPrompt();
 
 if(!process.argv[2]){
 	console.error("Usage: Node " + process.argv[1] + " serialport needed");
 	process.exit(1);
 };
 
-var myPort = new SerialPort(process.argv[2], {
-	parser: SerialPort.parsers.readline('\r\n')
+var port = new SerialPort(process.argv[2], {
+	parser: SerialPort.parsers.readline('\n')
 });
 
 io.on('connect', function(socket){
 	console.log('a user connected');
 });
 
-var data = createArray(1000,4);
-var i = 0;
-myPort.on('data', function(d){
-	console.log(d)
-	var reading = parseFloat(d);
+var data = new Array(1);
+var row = new Array(4);
 
-	console.log('reading:',reading);
-	if(reading<10000){
-		data[row][Math.floor(Math.trunc(reading)/1000)-1] = reading%1000;
-		console.log('row',row,data[row]);
-	}
-	else {
-		row++;
-	}
-});
+port.on('data', function(d){
 
-//=======================FOR WRITING TO FILE======================
-function writeToFile(){
-	row=0;
-		//Set up new file
-	fileName = name.concat(String(testNo)).concat('.csv');
-	var workbook = fileWriter.createWorkbook('./', fileName);
-	var sheet = workbook.createSheet('sheet1',6,1000);
-	sheet.set(1,1,'potato');
-	for(var i = 0;i<data.length;i++){
-		for(var j = 0;j<data[i].length;j++){
-			console.log(j+1);
-			console.log(i+1);
-			console.log(data[i][j]);
-			sheet.set(j+1,i+1,data[i][j]);
+	console.log(d);
+	var col = Math.floor(Math.trunc(d)/1000);
+	row[col-1] = d-(col*1000);
+	if(col == 4)
+		{
+			data.push(row);
+			console.log(row);
 		}
-	}
-
-	workbook.save(function(ok){
-		if (!ok) workbook.cancel();
-	});
-
-}
-
-//=========================FOR INPUTTING DATA TO ARDUINO====================
-// function setPrompt(){
-// 	prompt.get(['input'], function (err, result) {
-// 		if (err) { return onErr(err); }
-// 		console.log('Command-line input received: ' + result.input);
-// 		if(result.input==='start'){
-// 			myPort.write('y');
-// 			setPrompt();
-// 		}else if(result.input==='stop'){
-// 			writeToFile();
-// 			//stop loop and increment testNo
-// 			myPort.write('n');
-// 			testNo++;
-// 			setPrompt();
-// 		}
-// 	});
-// }
-
-function onErr(err) {
-	console.log(err);
-	return 1;
-}
-
-function createArray(length) {
-	var arr = new Array(length || 0),
-	i = length;
-
-	if (arguments.length > 1) {
-		var args = Array.prototype.slice.call(arguments, 1);
-		while(i--) arr[length-1 - i] = createArray.apply(this, args);
-	}
-
-	return arr;
-}
-
-
-function writeAndDrain (data, callback) {
-	sp.write(data, function () {
-		sp.drain(callback);
-	});
-}
-
+});
 
 app.get('/', function(req, res){
 	res.render('index');
@@ -125,89 +49,106 @@ server.listen(8000, function(){
 	console.log('listening on 8000');
 });
 
+//=======================FOR WRITING TO FILE======================
+function dataToString(rawdata){
+	var str = "";
+	//Converting data to string
+	for(var i = 1;i<rawdata.length;i++){
+		for(var j = 0; j<rawdata[i].length;j++){
+			str+=rawdata[i][j];
+			if(j<rawdata[i].length-1)
+				str+=',';
+		}
+		str+='\n';
+	}
+	return str;
+}
 
-// var SerialPort = require('serialport');
-// SerialPort.list(function (err, ports) {
-
-// 	ports.forEach(function(port) {
-
-// 		console.log(port.comName);
-// 		console.log(port.pnpId);
-// 		console.log(port.manufacturer);
-// 		if(port.manufacturer.includes('Arduino')){
-// 			var myPort = new SerialPort(port.comName, {
-// 				parser: SerialPort.parsers.readline('\n')
-// 			});
+function createCSVFile(data, name){
+	fs.writeFile(name+'.csv', data,  function(err) {
+		if (err) {
+			return console.error(err);
+		}
+	});
+}
 
 
-// 			io.on('connect', function(socket){
-// 				console.log('a user connected');
-// 			});
-// 			setPrompt();
+function downloadCSV(args, csvdata) {  
+	var data, filename, link;
+	var csv = dataToString(csvdata);
+	if (csv == null) return;
 
-// 			var data = createArray(1000,4);
-// 			var i = 0;
-// 			myPort.on('data', function(d){
-// 				var reading = parseFloat(d);
+	filename = args.filename || 'export.csv';
 
-// 				console.log('reading:',reading);
-// 				if(reading<10000){
-// 					data[row][Math.floor(Math.trunc(reading)/1000)-1] = reading%1000;
-// 					console.log('row',row,data[row]);
-// 				}
-// 				else {
-// 					row++;
-// 				}
-// 			});
-// 			//=======================FOR WRITING TO FILE======================
-// 			function writeToFile(){
-// 				row=0;
-//  	 			//Set up new file
-//   				fileName = name.concat(String(testNo)).concat('.csv');
-//   				var workbook = fileWriter.createWorkbook('./', fileName);
-//   				var sheet = workbook.createSheet('sheet1',6,1000);
-//   				sheet.set(1,1,'potato');
-//   				for(var i = 0;i<data.length;i++){
-//   					for(var j = 0;j<data[i].length;j++){
-//   						console.log(j+1);
-//   						console.log(i+1);
-//   						console.log(data[i][j]);
-//   						sheet.set(j+1,i+1,data[i][j]);
-//   					}
-//   				}
+	if (!csv.match(/^data:text\/csv/i)) {
+		csv = 'data:text/csv;charset=utf-8,' + csv;
+	}
+	data = encodeURI(csv);
 
-//   				workbook.save(function(ok){
-//   					if (!ok) 
-//   						workbook.cancel();
-//   					});
+	link = document.createElement('a');
+	link.setAttribute('href', data);
+	link.setAttribute('download', filename);
+	link.click();
+}
 
-//   				}
-// 			//=========================FOR INPUTTING DATA TO ARDUINO====================
-// 			function setPrompt(){
-// 				prompt.get(['input'], function (err, result) {
-// 					if (err) { return onErr(err); }
-// 					console.log('Command-line input received:');
-// 					console.log(result.input);
-// 					if(result.input==='start'){
-// 						myPort.write('y');
-// 						setPrompt();
-// 					}else if(result.input==='stop'){
-// 						writeToFile();
-// 						//stop loop and increment testNo
-// 						myPort.write('n');
-// 						testNo++;
-// 						setPrompt();
-// 					}
-// 				});
-// 			}
+function test(){
+	var outerarray = new Array(1);
+	var innerarray = new Array(4);
+	for(var i = 0;i<100;i++){
+		innerarray = new Array(4);
+		for(var j = 0;j<4;j++){
+			innerarray[j]=i;
+		}
+		outerarray.push(innerarray);
+	}
+	var string = dataToString(outerarray);
+	console.log(string);
+	createCSVFile(string,'potato');
+}
 
-// 			function onErr(err) {
-// 				console.log(err);
-// 				return 1;
-// 			}
-			
+//FOR CONVERTING BYTES TO FLOAT--------------UNUSED
+function Bytes2Float32(bytes) {
+	var sign = (bytes & 0x80000000) ? -1 : 1;
+	var exponent = ((bytes >> 23) & 0xFF) - 127;
+	var significand = (bytes & ~(-1 << 23));
 
-// 		}
-// 	});
-// });
+	if (exponent == 128) 
+		return sign * ((significand) ? Number.NaN : Number.POSITIVE_INFINITY);
+
+	if (exponent == -127) {
+		if (significand == 0) return sign * 0.0;
+		exponent = -126;
+		significand /= (1 << 22);
+	} else significand = (significand | (1 << 23)) / (1 << 23);
+
+	return sign * significand * Math.pow(2, exponent);
+}
+
+//=========================FOR INPUTTING DATA TO ARDUINO====================
+function setPrompt(){
+	prompt.get(['input'], function (err, result) {
+		if (err) { return onErr(err); }
+		console.log('Command-line input received:');
+		console.log(result.input);
+		if(result.input==='start'){
+			myPort.write('y');
+			setPrompt(myPort);
+		}else if(result.input==='stop'){
+ 			//stop loop and increment testNo
+ 			myPort.write('n');
+ 			testNo++;
+			setPrompt(myPort);
+		}else if(result.input === 'save'){
+			createCSVFile(dataToString(data),'potato');
+		}
+ 		});
+}
+
+function onErr(err) {
+	console.log(err);
+	return 1;
+}
+
+
+
 
