c0008d2206fcfffacf21b8a9bfd8b0c068102ce5
Michelle O'Bryan <Mobryan@dhcp-rhodes-1648.redrover.cornell.edu>
2017-02-15 10:57:23 -0500

    Rear motor header file

---
+++ controls/Main_2.0/RearMotor.h.ino
@@ -0,0 +1,17 @@
+#ifndef RearMotor_h
+#define RearMotor_h
+
+/*Define definite variables*/
+
+
+/*Define functions*/
+//get speed
+void getPeriod();
+//set speed
+void rampToPWM(float);
+//change direction
+void switchDirection();
+
+
+#endif //RearMotor_h
+
