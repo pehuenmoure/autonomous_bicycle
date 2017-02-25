094ccd08c8f91295f3d684e8613f8688fcff7926
Kenneth fang <kwf37@cornell.edu>
2017-02-24 23:38:57 -0500

    Deleted extraneous code in app.js. Also added a few comments

--- web/server/app.js
+++ web/server/app.js
@@ -6,24 +6,24 @@ var server = http.Server(app);
 var io = require('socket.io')(server);
 var SerialPort = require('serialport');
 var fs = require('fs');
-//For getting input- Will be edited out once site is nicer
-//var prompt = require('prompt');
-//prompt.start();
 
 app.set('view engine', 'ejs');
 
-//setPrompt();
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//For getting PortName
 if(!process.argv[2]){
 	console.error("Usage: Node " + process.argv[1] + " serialport needed");
 	process.exit(1);
 };
 
+//Initializing SerialPort Object
 var port = new SerialPort(process.argv[2], {
 	parser: SerialPort.parsers.readline('\n')
 });
 
+//Setting up array that stores incoming data
 var data = new Array(1);
 var row = new Array(4);
 
@@ -58,6 +58,7 @@ server.listen(8000, function(){
 	console.log('listening on 8000');
 });
 
+//For Rendering webpages
 app.get('/graph', (req, res) => {
     res.render('graph');
    });
@@ -81,6 +82,9 @@ app.get('/home', (req, res) => {
    });
 
 //=======================FOR WRITING TO FILE======================
+/*
+*Converts the 2d array into a csv String
+*/
 function dataToString(rawdata){
 	var str = "";
 	//Converting data to string
@@ -95,85 +99,15 @@ function dataToString(rawdata){
 	return str;
 }
 
+/*
+*Writes data to a .csv file
+*/
 function createCSVFile(data, name){
 	fs.writeFile(name+'.csv', data,  function(err) {
 		if (err) {
 			return console.error(err);
 		}
 	});
-}
-
-
-function downloadCSV(args, csvdata) {  
-	var data, filename, link;
-	var csv = dataToString(csvdata);
-	if (csv == null) return;
-
-	filename = args.filename || 'export.csv';
-
-	if (!csv.match(/^data:text\/csv/i)) {
-		csv = 'data:text/csv;charset=utf-8,' + csv;
-	}
-	data = encodeURI(csv);
-
-}
-
-function test(){
-	var outerarray = new Array(1);
-	var innerarray = new Array(4);
-	for(var i = 0;i<100;i++){
-		innerarray = new Array(4);
-		for(var j = 0;j<4;j++){
-			innerarray[j]=i;
-		}
-		outerarray.push(innerarray);
-	}
-	var string = dataToString(outerarray);
-	console.log(string);
-	createCSVFile(string,'potato');
-}
-
-//FOR CONVERTING BYTES TO FLOAT--------------UNUSED
-function Bytes2Float32(bytes) {
-	var sign = (bytes & 0x80000000) ? -1 : 1;
-	var exponent = ((bytes >> 23) & 0xFF) - 127;
-	var significand = (bytes & ~(-1 << 23));
-
-	if (exponent == 128) 
-		return sign * ((significand) ? Number.NaN : Number.POSITIVE_INFINITY);
-
-	if (exponent == -127) {
-		if (significand == 0) return sign * 0.0;
-		exponent = -126;
-		significand /= (1 << 22);
-	} else significand = (significand | (1 << 23)) / (1 << 23);
-
-	return sign * significand * Math.pow(2, exponent);
-}
-
-//=========================FOR INPUTTING DATA TO ARDUINO====================
-function setPrompt(){
-	prompt.get(['input'], function (err, result) {
-		if (err) { return onErr(err); }
-		console.log('Command-line input received:');
-		console.log(result.input);
-		if(result.input==='start'){
-			myPort.write('y');
-			setPrompt(myPort);
-		}else if(result.input==='stop'){
- 			//stop loop and increment testNo
- 			myPort.write('n');
- 			testNo++;
-			setPrompt(myPort);
-		}else if(result.input === 'save'){
-			createCSVFile(dataToString(data),'potato');
-		}
- 		});
-}
-
-function onErr(err) {
-	console.log(err);
-	return 1;
 }
 
 
