50e9f991a0a1b23ad289c382eb4f4b84e727c463
Fotini Kriezi <fk233@cornell.edu>
2017-03-02 00:07:24 -0500

    code structure

--- navigation/requestHandler.py
+++ navigation/requestHandler.py
@@ -3,25 +3,14 @@ import json
 import urllib
 import math
 
-""" Server needs to be open """
+""" Run server """
 
-#Gets url object and makes it into string
-url = urllib.urlopen("http://localhost:8000/secretdatatransfer")
-string = url.read()
-
-#Make into dictionary
-dictionary = json.loads(string)
-legs = dictionary["legs"] #points list
-
-#Gets latitude of first point
-print legs[0]["points"][0]['lat']
-
-def convert(longitude, latitude):
+def convert(latitude, longitude):
 	""" Mercator map projection 
 	http://stackoverflow.com/questions/14329691/covert-latitude-longitude-point-to-a-pixels-x-y-on-mercator-projection """
 
-	mapWidth    = 1000;
-	mapHeight   = 1000;
+	mapWidth    = 1000
+	mapHeight   = 1000
 
 	x = (longitude+180)*(mapWidth/360.0)
 
@@ -33,9 +22,53 @@ def convert(longitude, latitude):
 
 	return (x,y)
 
+def convert2(latitude, longitude):
+	"""http://stackoverflow.com/questions/1019997/convert-lat-longs-to-x-y-co-ordinates"""
+	mapWidth    = 1000
+	mapHeight   = 1000
 
-print convert(-76.4852, 42.45026)
-print convert(-76.48506, 42.450390000000006)
+	y = ((-1 * latitude) + 90) * (mapHeight / 180.0);
+	x = (longitude + 180) * (mapWidth / 360.0);
+
+	return (x, y)
+	
+# Also this --> http://stackoverflow.com/questions/1019997/convert-lat-longs-to-x-y-co-ordinates
+
+def parse_json():
+	#Gets url object and makes it into string
+	url = urllib.urlopen("http://localhost:8000/secretdatatransfer")
+	string = url.read()
+
+	#Make into dictionary
+	dictionary = json.loads(string)
+	legs = dictionary["legs"] #points list
+
+	points = []
+	points2 = []
+
+	#Accumulates list of point tuples
+	for i in legs:
+		for j in (i["points"]):
+			latitude = j['lat']
+			longitude = j['lng']
+			xy = convert(latitude, longitude)
+			xy2 = convert2(latitude, longitude)
+			points.append(xy)
+			points2.append(xy2)
+
+	print points
+	print points2
+
+
+parse_json()
+
+#Gets latitude of first point
+# print legs[0]["points"][0]['lat']
+
+
+
+# print convert(-76.4852, 42.45026)
+# print convert(-76.48506, 42.450390000000006)
 
 # LEG: A separate leg will be present for each waypoint or destination specified.
 # Each leg consists of a series of steps
@@ -51,4 +84,4 @@ print convert(-76.48506, 42.450390000000006)
 
 
 #Print string
-print len(legs)
+# print len(legs)
