e14299656ad4c1922d27c1b2a1bb525f3edcaf11
Michelle O'Bryan <Mobryan@dhcp-rhodes-2311.redrover.cornell.edu>
2017-03-08 17:58:36 -0500

    Added TinyGPS to RC_bike

--- RC_bike/RC_bike.ino
+++ RC_bike/RC_bike.ino
@@ -1,9 +1,13 @@
 #include "IMU.h"
 #include "PID.h"
 #include "RearMotor.h"
+#include "TinyGPS++.h"
 #include <math.h>
 
 /*Define definite variables*/
+//GPS 
+TinyGPSPlus gps;
+
 //Front Motor
 #define PWM_front 9
 #define DIR 46
@@ -245,6 +249,7 @@ void setup()
     attachInterrupt(RC_CH6, calcSignal6, CHANGE);
 
   Serial.begin(115200);
+  Serial3.begin(115200);
   initIMU();
   
   //setup rc
@@ -532,6 +537,20 @@ void loop() {
         delayMicroseconds(interval - l_diff);
       }else{
       Serial.println("LOOP LENGTH WAS VIOLATED. LOOP TIME WAS: " + String(l_diff));
+
+      //GPS output
+      while (Serial3.available() > 0) 
+        gps.encode(Serial3.read());
+        
+        Serial.print("Latitude                      "); Serial.println(gps.location.lat(), 6);
+        Serial.print("Longitude                     "); Serial.println(gps.location.lng(), 6);
+        Serial.print("Hours                         "); Serial.println(gps.time.hour()); // Hour (0-23)
+        Serial.print("Minutes                       "); Serial.println(gps.time.minute()); // Minute (0-59)
+        Serial.print("Seconds                       "); Serial.println(gps.time.second()); // Second (0-59)
+        Serial.print("Centiseconds                  "); Serial.println(gps.time.centisecond()); // 100ths of a second (0-99)
+        Serial.print("Date                          "); Serial.println(gps.date.value()); // Raw date in DDMMYY format
+        Serial.print("Number of Satelites in use    "); Serial.println(gps.satellites.value()); // Number of satellites in use
+        Serial.print("Course in degrees             "); Serial.println(gps.course.deg());
 
 //      while(true){}
     //}
