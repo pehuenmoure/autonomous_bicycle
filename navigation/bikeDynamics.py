10a346db8fcc31520dc19490321180219554b651
Fotini Kriezi <fk233@cornell.edu>
2017-03-18 21:01:10 -0400

    matlab bike dynamics in python, state structure and rhs function

---
+++ navigation/bikeDynamics.py
@@ -0,0 +1,63 @@
+import numpy as np
+import math
+from constants import *
+
+class MainNavigation(object):
+
+
+	def __init__(self, x, y):
+		"""INSTANCE ATTRIBUTES: 
+		x [float list]: x - coordinates of waypoints
+		y [float list]: y - coordinates of waypoints """
+		
+		self.x_coords = x
+		self.y_coords = y
+		self.tstep = TIMESTEP 
+		self.tarray = []
+		self.currentSegment = 1
+
+
+
+class State(object):
+
+	def __init__(self, xB, yB, phi, psi, delta, w_r, v):
+		""" State representation in Matlab """
+
+		self.xB = xB
+		self.yB = yB
+		self.phi = phi
+		self.psi = psi
+		self.delta = delta
+		self.w_r = w_r
+		self.v = v
+
+	
+	def rhs(self, steerD):
+		""" Modifies the state object to turn it into the next state """
+		deltaD = steerD
+		u = K1 * self.phi + K2 * self.w_r + K3 * (self.delta - deltaD)
+
+		if u>10:
+			u = 10
+		else:
+			u = -10
+
+		xdot = self.v * math.cos(self.psi)
+		ydot = self.v * math.sin(self.psi)
+		phi_dot = self.w_r
+		psi_dot = (self.v/L)*(math.tan(self.delta)/math.cos(self.phi))
+		delta_dot = u # ideal steer rate
+		v_dot = 0
+		wr_dot = (((-(self.v)**2)*self.delta) - B * self.v * u + G * L * self.phi)/(H*L)
+
+		# modifying state object 
+		self.xB = xdot
+		self.yB = ydot
+		self.phi = phi_dot
+		self.psi = psi_dot
+		self.delta = delta_dot
+		self.w_r = wr_dot
+		self.v = v_dot
+
+		return u
+
---
+++ navigation/constants.py
@@ -0,0 +1,25 @@
+# constants.py
+
+
+######### mainNavigation constants
+
+# properties of the p struct in matlab
+G = 9.81
+L = 1.02
+B = 0.5
+H = 0.5
+B = 0.3
+H = 0.9
+# trail is zero
+C = 0 
+
+# gains
+K1 = 71
+K2 = 21
+K3 = -20
+
+# time
+TIMESTEP = 1.0/100
+PAUSE = TIMESTEP
+
+
