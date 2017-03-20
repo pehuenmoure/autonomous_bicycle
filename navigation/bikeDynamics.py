6a780630e1d321082ed2d39727541ca34d8a3bd0
Fotini Kriezi <fk233@cornell.edu>
2017-03-20 01:55:06 -0400

    dynamic plotting, but mysterious bike trajectory

--- navigation/bikeDynamics.py
+++ navigation/bikeDynamics.py
@@ -41,7 +41,6 @@ class MainNavigation(object):
 		plt.show() 
 
 
-
 	def initialize_paths(self, waypoints):
 		""" returns list of paths from waypoints """
 		paths = []
@@ -51,6 +50,7 @@ class MainNavigation(object):
 			for i in range(1, len(waypoints)):
 				paths.append((waypoints[i-1], waypoints[i]))
 			return paths
+
 
 	def initialize_bike_state(self):
 		""" """
@@ -76,20 +76,27 @@ class MainNavigation(object):
 		# hl, = plt.plot([], [])
 		# plt.show()
 
-		while (k < 300):
+		while (k < 500):
 
 			xB = self.state.xB
 			yB = self.state.yB
 			phiB = self.state.phi
 
-			# Updating plot
+			# PLOT
+			# Bike trajectory
 			plt.scatter(xB, yB)
 			plt.show()
 			plt.pause(0.0001)
+
+			# Lean vs Time
+			# plt.scatter((k+1)*TIMESTEP, self.state.delta)
+			# plt.show()
+			# plt.pause(0.0001)
+
 			
 
 			#if the lean angle is too high, the test should count as a failure
-			if (abs(phiB) >= math.pi/4):
+			if (abs(phiB) >= math.pi/4.0):
 				print "Bike has fallen; test failure"
 				exit()
 
@@ -166,8 +173,8 @@ class State(object):
 		psi_dot = (self.v/L)*(np.tan(self.delta)/np.cos(self.phi))
 		delta_dot = u # ideal steer rate
 		v_dot = 0
-		wr_dot = (((-(self.v)**2)*self.delta) - B * self.v * u + G * L * self.phi)/(H*L)
-
+		# wr_dot = (((-(self.v)**2)*self.delta) - B * self.v * u + G * L * self.phi)/(H*L)
+		wr_dot = (1/H)*(G*np.sin(self.phi) - np.tan(self.delta)*((self.v**2)/L + B*v_dot/L + np.tan(self.phi)*((B/L)*self.v*phi_dot - (H/(L**2)*(self.v**2)*np.tan(self.delta))))-B*self.v*delta_dot/(L*np.cos(self.delta)**2))
 		# Returns u which is the motor command and the zdot vector in the form of a list
 		return (u, [xdot, ydot, phi_dot, psi_dot, delta_dot, wr_dot, v_dot])
 
--- navigation/constants.py
+++ navigation/constants.py
@@ -6,21 +6,19 @@ import math
 # properties of the p struct in matlab
 G = 9.81
 L = 1.02
-B = 0.5
-H = 0.5
 B = 0.3
 H = 0.9
 # trail is zero
 C = 0 
 
 # gains
-K1 = 71
-K2 = 21
-K3 = -20
+K1 = 71.
+K2 = 21.
+K3 = -20.
 
 # time
 TIMESTEP = 1.0/100
 PAUSE = TIMESTEP
 
 # used in converting output of nav to steerD
-MAX_STEER = math.pi/6
+MAX_STEER = math.pi/6.0
