c2b78fc3a8e8bf2875ddf0d956fe5d04ed1197d4
Fotini Kriezi <fk233@cornell.edu>
2017-03-25 14:20:21 -0400

    new nav files changes

--- navigation/new_nav/mapModel.py
+++ navigation/new_nav/mapModel.py
@@ -6,7 +6,6 @@ class Map_Model(object):
 
 	def __init__(self, bike, waypoints, obstacles, paths = []):
 		""" Initializes the Map Model """
-
 		self.bike = bike
 		self.paths = paths
 		self.waypoints = waypoints
@@ -15,7 +14,6 @@ class Map_Model(object):
 
 	def add_path(self, p1, p2):
 		""" Adds a new path from point p1 to point p2 at the end of the path list """
-
 		self.paths.append([p1,p2])
 		self.waypoints[0].append(p1[0])
 		self.waypoints[0].append(p2[0])
@@ -27,7 +25,6 @@ class Map_Model(object):
 		""" Adds a new point p to the list of waypoints. If it is not the first point added in 
 		the waypoints list, then a path is also added from the last point in waypoints to the 
 		new point p that we add """
-		
 		if (len(self.paths) != 0):
 			previous_point = self.paths[-1][1]
 			self.paths.append([previous_point, p])
@@ -40,13 +37,11 @@ class Map_Model(object):
 
 	def close_path(self):
 		""" Adds a path from the last point to the first point in the waypoints list """
-
 		self.add_point(self.paths[0][0])
 
 
 	def draw_circle(self, center, r, n_points, degrees = 2*np.pi):
 		""" Draws a circle with given characteristics """
-
 		deg_inc = float(degrees)/n_points
 		theta = deg_inc
 		p0 = np.array(center) + np.array([r, 0])
--- navigation/new_nav/nav.py
+++ navigation/new_nav/nav.py
@@ -2,6 +2,7 @@
 import numpy as np
 import math
 import sys
+import geometry
 
 class Nav(object):
 	"""INSTANCE ATTRIBUTES:
@@ -15,12 +16,12 @@ class Nav(object):
 
 
 	def direction_to_turn(self):
-		""""""
+		""" Returns: the steer command """
 		self.target_path = self.find_closest_path()
-		distance = np.abs(self.distance_from_path())
+		bike_pos = (self.map_model.bike.xB, self.map_model.bike.yB)
+		distance = np.abs(geometry.distance_from_path(bike_pos, self.map_model.paths[self.target_path]))
 		delta = np.abs(self.displacement_to_turn())
 		delta = delta + 10 # VALUE I calculated using my overshoot calculation functions
-
 		if delta<distance:
 			return self.turn_perp()
 		else:
@@ -31,15 +32,66 @@ class Nav(object):
 		""" Finds and returns the closest path to the bike from the list of paths """
 		closest_distance = sys.maxint
 		closest_path = 0
+		bike_position = (self.map_model.bike.xB, self.map_model.bike.yB)
 		for path_index in range(len(self.map_model.paths)):
-			bike_position = (self.map_model.bike.xB, self.map_model.bike.yB)
 			nearest_point = geometry.nearest_point_on_path(self.map_model.paths[path_index], bike_position)
 			distance_to_bike = geometry.dist(nearest_point, bike_position)
 			if (closest_distance > distance_to_bike):
 				closest_distance = distance_to_bike
-				closest_path = path_index # CONTINUE HERE
+				closest_path = path_index 
 		disp_next = self.displacement_to_turn(target_path = (closest_path+1)%len(self.map_model.paths))
-		distance_next = self.distance_from_path((closest_path+1)%len(self.map_model.paths))
+		target_path = (closest_path+1)%len(self.map_model.paths)
+		distance_next = geometry.distance_from_path(bike_position, self.map_model.paths(target_path))
 		if disp_next - np.abs(distance_next)>-0.01:
 			closest_path = np.mod(closest_path + 1,len(self.map_model.paths))
 		return closest_path
+
+
+
+	def displacement_to_turn(self, b = None, target_path = None):
+		""" Returns: the delta which represents the distance at which the bike 
+		should enter the s curve based on its turning radius """
+		if target_path is None:
+			target_path = self.target_path
+		p = geometry.unit_vector(self.map_model.paths[target_path])
+		if b is None:
+			b = self.map_model.bike.vector
+		R = np.array([[p[0], p[1]], [-p[1], p[0]]])
+		p_R = R.dot(p)
+		b_R = R.dot(b)
+		phi = (np.arccos(b_R[0])-np.pi/2)%(2*np.pi)
+		r = self.map_model.bike.turning_r
+		delta = r+r*np.sin(phi)
+		return delta
+
+
+	def turn_parallel(self):
+		""""""
+		path_vector = geometry.unit_vector(self.map_model.paths[self.target_path])
+		path_perp = np.array([-path_vector[1], path_vector[0]])
+		return self.turn_helper(path_perp)
+
+
+	def turn_perp(self):
+		""""""
+		path_vector = geometry.unit_vector(self.map_model.paths[self.target_path])
+		p_orig = path_vector
+		bike_pos = (self.map_model.bike.xB, self.map_model.bike.yB)
+		sign = geometry.get_sign(geometry.distance_from_path(bike_pos, self.map_model.paths[self.target_path]))
+		path_vector = path_vector*sign
+		return self.turn_helper(path_vector)
+
+
+	def turn_helper(self, path_vector):
+		""""""
+		bike_vector = self.map_model.bike.vector
+		dot_product = np.sum(bike_vector*path_vector)
+		turn = 0 if np.abs(dot_product)<.01 else geometry.get_sign(dot_product)*(-1)
+		facing_away = np.sum(np.array([-path_vector[1], path_vector[0]])*bike_vector)>0
+		if turn == 0 and facing_away:
+			return 1
+		return turn
+
+
+
+
